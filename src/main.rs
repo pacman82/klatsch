ce869a42351e6523a4dba8ac2e9372ded3cc6391
Markus Klein <markus-klein@live.de>
2026-01-22 16:54:29 +0100

    feat: Minimal server displaying "Hello, World!"

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use axum::{Router, routing::get};
+use dotenv::dotenv;
+use tokio::net::TcpListener;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> anyhow::Result<()> {
+    // Load '.env' file; Ignore errors.
+    dotenv().ok();
+
+    let endpoint = std::env::var("ENDPOINT").unwrap_or_else(|_| "0.0.0.0:3000".to_string());
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = TcpListener::bind(endpoint).await?;
+    axum::serve(listener, app)
+        .await
+        .expect("axum::serve never completes");
+    Ok(())
 }
