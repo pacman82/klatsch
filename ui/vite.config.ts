57648fac933be5f69d3af8f160f1d19a3ea514a5
Markus Klein <markus-klein@live.de>
2026-01-29 13:26:25 +0100

    build: npm run dev works now with Rust backend in seperate process

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,6 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Dev server options
+	server: {
+		// During production, the backend and frontend are served by the same process. In order to support
+		// hot reloading during development, we want to use `npm run dev` while working on the frontend.
+		// To have all the functionality we need we still run the rust backend server.
+		proxy: {
+			'/': backend_url_for_dev()
+		}
+	}
 });
+
+// Rust backend URL during development.
+function backend_url_for_dev() {
+	const backendHost = process.env.HOST || '127.0.0.1';
+	const backendPort = process.env.PORT || '3000';
+	return `http://${backendHost}:${backendPort}`;
+}
