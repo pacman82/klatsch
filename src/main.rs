d5ae4365336f2d8cb0192948a3e2328481e13fb8
Markus Klein <markus-klein@live.de>
2026-01-31 23:43:38 +0100

    chore: Place todo for there to go next

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,8 @@ async fn main() -> anyhow::Result<()> {
     // Gracefully shutdown the http server.
     server.shutdown().await;
 
+    // TODO: Shutdown even if clients are still connected and have the UI open.
+
     // Let's shutdown the conversation runtime as well. After the http interface, since the http
     // interface relies on it.
     conversation.shutdown().await;
