4c62ccfa9ccaac17a6e905be776540d4855e742b
Markus Klein <markus-klein@live.de>
2026-02-07 23:55:47 +0100

    test: Integration tests now for "Ready" to be logged in order to determine readiness.

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,8 +5,9 @@ use std::{
 
 use reqwest::Client;
 use tokio::{
-    process::{Child, Command},
-    time::sleep,
+    io::{AsyncBufReadExt, BufReader, Lines},
+    process::{Child, ChildStderr, Command},
+    time::timeout,
 };
 
 #[cfg(unix)]
@@ -25,7 +26,6 @@ use nix::{
 async fn server_shuts_down_within_1_sec() {
     // Given a running server
     let mut child = TestServer::new(3001).await;
-    child.wait_for_health_check().await;
 
     // When sending SIGTERM to the server process
     child.send_sigterm();
@@ -52,7 +52,6 @@ async fn server_shuts_down_within_1_sec() {
 async fn server_finished_with_success_status_code_after_terminate() {
     // Given a runninng server process
     let mut child = TestServer::new(3002).await;
-    child.wait_for_health_check().await;
 
     // When sending SIGTERM to the server process
     child.send_sigterm();
@@ -70,13 +69,27 @@ async fn server_finished_with_success_status_code_after_terminate() {
 async fn server_boots_within_one_sec() {
     // Given a start time
     let start = Instant::now();
+
     // When measuring the time it takes to boot
-    let mut child = TestServer::new(3003).await;
-    child.wait_for_health_check().await;
+    let _child = TestServer::new(3003).await;
     let end = Instant::now();
+
     // Then it should have taken less than 1 second to boot up
     let max_duration = Duration::from_secs(1);
     assert!(end - start <= max_duration)
+}
+
+#[tokio::test]
+async fn health_check_returns_200_ok() {
+    // Given a running server
+    let server = TestServer::new(3005).await;
+
+    // When requesting the health check endpoint
+    let response = server.health_check().await;
+
+    // Then it should return 200 OK
+    assert_eq!(response.status(), 200);
+    assert_eq!(response.text().await.unwrap(), "OK");
 }
 
 #[cfg(not(windows))]
@@ -84,7 +97,6 @@ async fn server_boots_within_one_sec() {
 async fn shutdown_within_1_sec_with_active_events_stream_client() {
     // Given a running server
     let mut child = TestServer::new(3004).await;
-    child.wait_for_health_check().await;
 
     // and a client connected to the events stream
     let _event_stream_body = child
@@ -121,7 +133,10 @@ struct TestServer {
 
 impl TestServer {
     async fn new(port: u16) -> Self {
-        let process = ServerProcess::new(port);
+        let mut process = ServerProcess::new(port);
+        timeout(Duration::from_secs(5), process.wait_for_ready())
+            .await
+            .expect("Server did not become ready within 5 seconds");
         let client = Client::new();
         Self {
             process,
@@ -130,19 +145,12 @@ impl TestServer {
         }
     }
 
-    async fn wait_for_health_check(&mut self) {
-        tokio::time::timeout(Duration::from_secs(5), async {
-            while let Err(_) = self
-                .client
-                .get(format!("http://localhost:{}/health", self.port))
-                .send()
-                .await
-            {
-                sleep(Duration::from_millis(10)).await;
-            }
-        })
-        .await
-        .expect("Server did not become healthy within 5 seconds");
+    async fn health_check(&self) -> reqwest::Response {
+        self.client
+            .get(format!("http://localhost:{}/health", self.port))
+            .send()
+            .await
+            .expect("Failed to send health check request")
     }
 
     // Supported on every platform, but so far only used in unix-specific tests.
@@ -173,21 +181,47 @@ impl TestServer {
 /// scope.
 struct ServerProcess {
     child: Child,
+    // We use this to wait for the "Ready" log line to appear. We need to keep stderr alive beyond
+    // this, even if we should no longer be interested in the log output, because otherwise the
+    // server process panics trying to write to a closed stderr.
+    //
+    // Current shortcoming: If the server process writes a lot to stderr, it could fill up the
+    // buffer and block the process. In practice this is currently not an issue, because the server
+    // only writes a few log lines during startup and shutdown, but it is something to keep in mind
+    // for the future.
+    stderr_lines: Lines<BufReader<ChildStderr>>,
 }
 
 impl ServerProcess {
     pub fn new(port: u16) -> Self {
         let binary_path = env!("CARGO_BIN_EXE_klatsch");
-        let child = Command::new(binary_path)
+        let mut child = Command::new(binary_path)
             .env("PORT", port.to_string())
             // We do not want the log output of the process to clutter the output of our test
             // runner.
             .stdout(Stdio::null())
-            .stderr(Stdio::null())
+            .stderr(Stdio::piped())
             .spawn()
             .unwrap();
+        let stderr = child.stderr.take().unwrap();
+        let stderr_lines = BufReader::new(stderr).lines();
 
-        Self { child }
+        Self {
+            child,
+            stderr_lines,
+        }
+    }
+
+    /// Waits for a log line containing "Ready" to appear in stderr.
+    /// Returns an error if the process terminates before the line is found.
+    pub async fn wait_for_ready(&mut self) {
+        while let Some(line) = self.stderr_lines.next_line().await.unwrap() {
+            if line.contains("Ready") {
+                return;
+            }
+        }
+
+        panic!("Process stderr closed before 'Ready' log line appeared",)
     }
 
     #[cfg(unix)]
