61cb3e400cfdc13e9826e1721ac9235478d8f188
Markus Klein <markus-klein@live.de>
2026-02-19 17:11:07 +0100

    chore: Update to memory-serve 2

--- src/server/ui.rs
+++ src/server/ui.rs
@@ -1,10 +1,9 @@
 //! Module for statically hosting the UI assets
 
 use axum::Router;
-use memory_serve::{MemoryServe, load_assets};
 
 pub fn ui_router() -> Router {
-    MemoryServe::new(load_assets!("./ui/build"))
+    memory_serve::load!()
         .index_file(Some("/index.html"))
         .into_router()
 }
