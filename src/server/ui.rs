519a3720317e912ee584254fbc9106f25e8d3daa
Markus Klein <markus-klein@live.de>
2026-03-02 11:20:04 +0100

    refactor: Repalce memory_serve with static_serve

--- src/server/ui.rs
+++ src/server/ui.rs
@@ -1,9 +1,12 @@
 //! Module for statically hosting the UI assets
 
 use axum::Router;
+use static_serve::embed_assets;
+
+// `strip_html_ext` stirps `.html` extension from `index.html` and serves it as root `/` path.
+embed_assets!("ui/build", strip_html_ext = true);
 
 pub fn ui_router() -> Router {
-    memory_serve::load!()
-        .index_file(Some("/index.html"))
-        .into_router()
+    // static_router method is created by embed_assets macro.
+    static_router()
 }
