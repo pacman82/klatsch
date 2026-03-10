07080088bf8691ff34f7ba8e199d5e7ce5e6d2a5
Markus Klein <markus-klein@live.de>
2026-03-10 10:47:44 +0100

    feat: Map axum::serve target to server in log output

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,11 +1,16 @@
+mod format;
+
 use std::io::stderr;
 
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
+use self::format::OperatorFormat;
+
 pub fn init_tracing() {
     let subscriber = FmtSubscriber::builder()
         .with_writer(stderr)
+        .event_format(OperatorFormat)
         .with_env_filter(
             EnvFilter::builder()
                 .with_env_var("LOG_LEVEL")
@@ -16,4 +21,17 @@ pub fn init_tracing() {
         .finish();
     tracing::subscriber::set_global_default(subscriber)
         .expect("Setting global default provider must not fail.");
+}
+
+/// Maps the module string to an operator friendly target.
+///
+/// The rust tracinig ecosystem uses the module path as the default target for log messages. This
+/// function allows us to map `axum::serve` to `server` or `tower_http::trace::on_request` to
+/// `http`. I.e. we can hide our internal module structure and dependencies and translate it into
+/// meaningful categories for an operator.
+fn operator_target(target: &'static str) -> &'static str {
+    match target {
+        "axum::serve" => "server",
+        other => other,
+    }
 }
