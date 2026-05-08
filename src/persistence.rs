13ff34d371af109558643e398b3a179680aad53f
Markus Klein <markus-klein@live.de>
2026-05-08 11:48:16 +0200

    test: Test schema creation from scratch

--- src/persistence.rs
+++ src/persistence.rs
@@ -65,3 +65,46 @@ pub trait ExecuteSql {
 pub trait PersistenceError {
     fn is_unique_constraint_violation(&self) -> bool;
 }
+
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+
+    use super::{Persistence as _, SqlitePersistence};
+    use crate::chat::create_schema_chat;
+
+    #[tokio::test]
+    async fn create_scheam_from_scratch() {
+        // Given an empty persistence directory
+        let dir = tempdir().unwrap();
+
+        // When starting persistence in this directory
+        let persistence = SqlitePersistence::new(Some(dir.path()), create_schema_chat)
+            .await
+            .unwrap();
+
+        // Then the schema should be created
+        let expected_sql = vec![
+            "CREATE TABLE events (\n                    \
+            id INTEGER PRIMARY KEY,\n                    \
+            message_id BLOB UNIQUE NOT NULL,\n                    \
+            sender TEXT NOT NULL,\n                    \
+            content TEXT NOT NULL,\n                    \
+            timestamp_ms INTEGER NOT NULL\n                \
+            )",
+        ];
+
+        let sql = persistence
+            .rows_vec(
+                "SELECT sql FROM sqlite_schema WHERE type = 'table' ORDER BY name",
+                (),
+                |row| {
+                    let sql: Option<String> = row.get(0).unwrap();
+                    Ok(sql.unwrap())
+                },
+            )
+            .await
+            .unwrap();
+        assert_eq!(expected_sql.as_slice(), &sql);
+    }
+}
