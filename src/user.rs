9a248b4bc9ff26bcc72051b1aa3e7d24efc8b209
Markus Klein <markus-klein@live.de>
2026-06-16 08:37:39 +0200

    test: Test for User::user_id

--- src/user.rs
+++ src/user.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::persistence::ExecuteSql;
 
 pub struct Users;
@@ -12,9 +14,21 @@ impl Users {
 pub struct UsersClient;
 
 #[cfg_attr(test, double_trait::dummies)]
-pub trait Authenticate {}
+pub trait Authenticate {
+    fn user_id(&mut self, name: String) -> impl Future<Output = Result<Uuid, AuthenticationError>>;
+}
 
-impl Authenticate for UsersClient {}
+impl Authenticate for UsersClient {
+    async fn user_id(&mut self, name: String) -> Result<Uuid, AuthenticationError> {
+        let uuid = Uuid::new_v4();
+        Ok(uuid)
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthenticationError {
+    Internal,
+}
 
 pub fn migrate_users_persistence<C>(conn: &C, from_version: u32) -> Result<(), C::Error>
 where
@@ -42,4 +56,32 @@ where
         (),
     )?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Authenticate, Users};
+
+    #[tokio::test]
+    async fn different_uuids_for_each_user() {
+        let users = Users;
+        let mut client = users.client();
+
+        let alice_id = client.user_id("Alice".to_owned()).await.unwrap();
+        let bob_id = client.user_id("Bob".to_owned()).await.unwrap();
+
+        assert_ne!(alice_id, bob_id)
+    }
+
+    #[tokio::test]
+    #[should_panic] // Not implemented yet
+    async fn same_user_always_has_same_uuid() {
+        let users = Users;
+        let mut client = users.client();
+
+        let alice_id_1 = client.user_id("Alice".to_owned()).await.unwrap();
+        let alice_id_2 = client.user_id("Alice".to_owned()).await.unwrap();
+
+        assert_eq!(alice_id_1, alice_id_2)
+    }
 }
