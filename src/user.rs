7aad33e116fc1c9ebdd1f0593dc2ee977d79fe39
Markus Klein <markus-klein@live.de>
2026-06-16 09:45:24 +0200

    refactor: Users persists user ids

--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,6 @@
 use uuid::Uuid;
 
-use crate::persistence::ExecuteSql;
+use crate::persistence::{ExecuteSql, FieldAccess as _, Persistence};
 
 #[derive(Clone)]
 pub struct Users<P> {
@@ -18,9 +18,16 @@ pub trait Authenticate {
     fn user_id(&mut self, name: String) -> impl Future<Output = Result<Uuid, AuthenticationError>>;
 }
 
-impl<P> Authenticate for Users<P> {
+impl<P> Authenticate for Users<P>
+where
+    P: Persistence,
+{
     async fn user_id(&mut self, name: String) -> Result<Uuid, AuthenticationError> {
-        let uuid = Uuid::new_v4();
+        let uuid = self
+            .persistence
+            .transaction(|conn| fetch_user_id(conn, name))
+            .await
+            .map_err(|_| AuthenticationError::Internal)?;
         Ok(uuid)
     }
 }
@@ -28,6 +35,30 @@ impl<P> Authenticate for Users<P> {
 #[derive(Debug)]
 pub enum AuthenticationError {
     Internal,
+}
+
+fn fetch_user_id<C>(conn: &C, name: String) -> Result<Uuid, C::Error>
+where
+    C: ExecuteSql,
+{
+    let maybe_user_id = conn
+        .rows_vec("SELECT id FROM users WHERE name = ?1", &name, |row| {
+            Ok(row.get_uuid(0))
+        })?
+        .pop();
+    let user_id = match maybe_user_id {
+        Some(user_id) => user_id,
+        None => {
+            let user_id = Uuid::new_v4();
+            conn.execute(
+                "INSERT INTO users (id, name) VALUES (?1, ?2)",
+                (&user_id, &name),
+            )?;
+            user_id
+        }
+    };
+
+    Ok(user_id)
 }
 
 pub fn migrate_users_persistence<C>(conn: &C, from_version: u32) -> Result<(), C::Error>
@@ -60,13 +91,17 @@ where
 
 #[cfg(test)]
 mod tests {
-    use double_trait::Dummy;
+    use crate::{
+        persistence::{Persistence, SqlitePersistence},
+        user::migrate_users_persistence,
+    };
 
     use super::{Authenticate, Users};
 
     #[tokio::test]
     async fn different_uuids_for_each_user() {
-        let mut users = Users::new(Dummy);
+        let persistence = persistence_fake().await;
+        let mut users = Users::new(persistence);
 
         let alice_id = users.user_id("Alice".to_owned()).await.unwrap();
         let bob_id = users.user_id("Bob".to_owned()).await.unwrap();
@@ -75,13 +110,19 @@ mod tests {
     }
 
     #[tokio::test]
-    #[should_panic] // Not implemented yet
     async fn same_user_always_has_same_uuid() {
-        let mut users = Users::new(Dummy);
+        let persistence = persistence_fake().await;
+        let mut users = Users::new(persistence);
 
         let alice_id_1 = users.user_id("Alice".to_owned()).await.unwrap();
         let alice_id_2 = users.user_id("Alice".to_owned()).await.unwrap();
 
         assert_eq!(alice_id_1, alice_id_2)
+    }
+
+    async fn persistence_fake() -> impl Persistence {
+        SqlitePersistence::new(None, migrate_users_persistence)
+            .await
+            .unwrap()
     }
 }
