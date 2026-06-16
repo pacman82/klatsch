da9e9565ab8e1065c7a05d617eea8d7e549ee3ca
Markus Klein <markus-klein@live.de>
2026-06-16 09:02:42 +0200

    refactor: Move tests for migration into dedicated module

--- src/persistence/migrate.rs
+++ src/persistence/migrate.rs
@@ -12,3 +12,52 @@ where
     migrate_chat_persistence(conn, from_version)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+
+    use tempfile::tempdir;
+    use tokio::fs;
+
+    use crate::persistence::{Persistence as _, SqlitePersistence};
+
+    use super::migrate;
+
+    #[tokio::test]
+    async fn schema_from_v1() {
+        // Given an persistence directory with an existing v1 database
+        let dir = tempdir().unwrap();
+        fs::copy("tests/v1.db", dir.path().join("klatsch.db"))
+            .await
+            .unwrap();
+
+        // When starting persistence in this directory
+        let persistence = SqlitePersistence::new(Some(dir.path()), migrate)
+            .await
+            .unwrap();
+
+        // Then
+        assert_eq!(sql_schema_from_scratch().await, schema(&persistence).await)
+    }
+
+    async fn schema(persistence: &SqlitePersistence) -> Vec<String> {
+        persistence
+            .rows_vec(
+                "SELECT sql FROM sqlite_schema WHERE type = 'table' ORDER BY name",
+                (),
+                |row| {
+                    let sql: Option<String> = row.get(0).unwrap();
+                    Ok(sql.unwrap())
+                },
+            )
+            .await
+            .unwrap()
+    }
+
+    /// SQL creating the the persistence schema from scratch, then no migration takes place.
+    async fn sql_schema_from_scratch() -> Vec<String> {
+        let persistence = SqlitePersistence::new(None, migrate).await.unwrap();
+
+        schema(&persistence).await
+    }
+}
