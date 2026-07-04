be17da103a3455626317015ea76a987625c41d0a
Markus Klein <markus-klein@live.de>
2026-07-04 15:21:57 +0200

    refactor: UserId extracted into own module

---
+++ src/user/user_id.rs
@@ -0,0 +1,46 @@
+use std::{fmt, str::FromStr};
+
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::persistence::{Argument, AsArgument};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct UserId(Uuid);
+
+impl UserId {
+    pub const fn from_uuid(uuid: Uuid) -> Self {
+        UserId(uuid)
+    }
+
+    pub fn new() -> Self {
+        Self::from_uuid(Uuid::new_v4())
+    }
+}
+
+impl fmt::Display for UserId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl FromStr for UserId {
+    type Err = uuid::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse().map(UserId)
+    }
+}
+
+impl AsArgument for UserId {
+    fn as_argument(&self) -> Argument<'_> {
+        self.0.as_argument()
+    }
+}
+
+impl AsArgument for &UserId {
+    fn as_argument(&self) -> Argument<'_> {
+        (&self.0).as_argument()
+    }
+}
