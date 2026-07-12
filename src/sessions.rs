0a68e886f215f2cc9239bb22f6041b4b94f839e1
Markus Klein <markus-klein@live.de>
2026-07-12 20:30:01 +0200

    refactor: AuthenticatedUser is now defined in session_http.rs

--- src/sessions.rs
+++ src/sessions.rs
@@ -1,10 +1,12 @@
+mod session_http;
 mod session_id;
 mod session_store;
 mod sessions_runtime;
 
 pub use self::{
+    session_http::AuthenticatedUser,
     session_id::SessionId,
-    sessions_runtime::{SessionLookup, SessionLifecycle, SessionsRuntime},
+    sessions_runtime::{SessionLifecycle, SessionLookup, SessionsRuntime},
 };
 
 use self::session_store::{InMemorySessionStore, SessionStore};
