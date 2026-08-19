dd5d4a8dec51ab9f310f2debd14cdf1eab88a687
Markus Klein <markus-klein@live.de>
2026-08-19 21:20:21 +0200

    fix: Signal handlers are registerd before emitting "ready"

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -1,25 +1,24 @@
+use futures_util::FutureExt;
 use tokio::signal::ctrl_c;
 
 /// Registers signal handlers for termination and interrupt signals. I.e. this application will
-/// gracefully shutdown with Ctrl+C as well as container stop.
-///
-/// Awaiting the result of this function will return a future which completes if a signal to
-/// shutdown is received. I.e. after the first call to `await` the signal handlers are registered.
-/// The second call to `await` waits for the signal itself.
-pub async fn shutdown_signal() -> impl Future<Output = ()> {
-    let ctrl_c = async {
-        ctrl_c().await.expect("failed to install Ctrl+C handler");
-    };
+/// gracefully shutdown with Ctrl+C as well as container stop. The returned future completes as soon
+/// as a signal is received.
+pub fn shutdown_signal() -> impl Future<Output = ()> {
+    // We are careful to install the the ctrl+c handler synchronously. We achieve this by wrapping
+    // the resulting future using `map`, rather than `await`ing it in an async block and wrapping
+    // the result.
+    let ctrl_c = ctrl_c().map(|result| result.expect("failed to install Ctrl+C handler"));
 
     #[cfg(unix)]
     use tokio::signal::unix;
 
     #[cfg(unix)]
-    let terminate = async {
-        unix::signal(unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
+    let terminate = {
+        let mut signal =
+            unix::signal(unix::SignalKind::terminate()).expect("failed to install signal handler");
+
+        async move { signal.recv().map(|_| ()).await }
     };
 
     #[cfg(not(unix))]
