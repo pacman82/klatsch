fb38b905b464fcb843c4e64361160261c28bcddf
Markus Klein <markus-klein@live.de>
2026-08-19 19:42:47 +0200

    refactor: AuthenticateUser::login return AuthenticationError

--- src/sessions.rs
+++ src/sessions.rs
@@ -9,7 +9,7 @@ pub use self::{
     session_id::SessionId,
     session_persistence::migrate_session_persistence,
     session_store::SessionExpiry,
-    sessions_runtime::{SessionLifecycle, AuthenticateSession, SessionsRuntime},
+    sessions_runtime::{AuthenticateSession, SessionLifecycle, SessionsRuntime},
 };
 
 use self::{
