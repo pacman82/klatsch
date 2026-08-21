45be33671dcb0e2538f8821428ba54ccf3cf7ad2
Markus Klein <markus-klein@live.de>
2026-08-21 09:28:29 +0200

    refactor: login_routes are private to authentication module

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,7 +1,33 @@
 mod auth_service;
 mod authenticate_request;
+mod session_cookie;
+
+use tokio::sync::watch;
+
+use crate::{
+    server::Routes,
+    sessions::SessionLifecycle,
+    user::{AuthenticateUser, Users},
+};
+
+use self::{auth_service::Login, session_cookie::login_routes};
 
 pub use self::{
-    auth_service::{AuthService, Login},
+    auth_service::AuthService,
     authenticate_request::{AuthenticateRequest, AuthenticatedUser},
 };
+
+impl<U, S> Routes for AuthService<U, S>
+where
+    U: Send + Sync + Clone + AuthenticateUser + Users + 'static,
+    S: Send + Sync + Clone + SessionLifecycle + 'static,
+{
+    fn routes(
+        self,
+        _auth: impl AuthenticateRequest + Send + Sync + Clone + 'static,
+        _shutting_down: watch::Receiver<bool>,
+        encrypted: bool,
+    ) -> axum::Router<()> {
+        login_routes(self, encrypted)
+    }
+}
