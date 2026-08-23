3cedd87917c905f292b3f8e57d508b30bb72ff70
Markus Klein <markus-klein@live.de>
2026-08-23 13:17:20 +0200

    refactor: AuthService constructions bubbled up to router

--- src/server/api.rs
+++ src/server/api.rs
@@ -1,27 +1,24 @@
 use crate::{
-    authentication::{AuthService, AuthenticateRequest},
+    authentication::AuthenticateRequest,
     invites::invite_routes,
     server::Routes,
-    sessions::SessionLifecycle,
     user::{AuthenticateUser, Users},
 };
 use axum::Router;
 use tokio::sync::watch;
 
-pub fn api_router<C, U, S>(
+pub fn api_router<C, U, A>(
     chat: C,
     users: U,
-    sessions: S,
+    auth_service: A,
     shutting_down: watch::Receiver<bool>,
     encrypted: bool,
 ) -> Router
 where
     C: Routes,
     U: Users + AuthenticateUser + Routes + Send + Sync + Clone + 'static,
-    S: SessionLifecycle + AuthenticateRequest + Send + Sync + Clone + 'static,
+    A: AuthenticateRequest + Routes + Send + Sync + Clone + 'static,
 {
-    let auth_service = AuthService::new(users.clone(), sessions.clone());
-
     Router::new()
         .merge(
             auth_service
